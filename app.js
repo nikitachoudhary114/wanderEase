72dce2fd1bca608306b537668a5ff87e9feb4469
Nikita Choudhary <nikitachoudhary364@gmail.com>
2024-07-06 15:05:59 +0530

    v1.2

--- app.js
+++ app.js
@@ -103,6 +103,8 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+
+
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
